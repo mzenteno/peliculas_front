0ad5c506e8a744cacc911bb3730f09b5a396eccb
mzenteno.intedes@gmail.com <mzenteno.intedes@gmail.com>
2024-09-04 20:08:44 -0400

    cambios en rutas relativas

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import imgLogo from "/img/logo.svg";
-import styles from '@components/navbar/Navbar.module.css';
+// import imgLogo from "img/logo.svg";
+import styles from "@components/navbar/Navbar.module.css";
 
 const Logo = () => {
   return (
     <a href="index.html" className={styles.header__logo}>
-      <img src={imgLogo} alt="" />
+      <img src="/img/logo.svg" alt="" />
     </a>
   );
 };

91b64a20b4e62ee7b35ee2886bfaddf1be06f4c3
mzenteno.intedes@gmail.com <mzenteno.intedes@gmail.com>
2024-09-04 21:20:30 -0400

    cambio 1 para rutas relativas

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Home from "@components/home/Home";
 import Movies from "@components/movies/Movies";
 import Navbar from "@components/navbar/Navbar";
 import Detail from "@components/detail/Detail";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />}></Route>
         <Route path="/movies" element={<Movies />}></Route>
         <Route path="/detail/:id" element={<Detail />}></Route>
--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-// import imgLogo from "img/logo.svg";
 import styles from "@components/navbar/Navbar.module.css";
 
 const Logo = () => {
   return (
     <a href="index.html" className={styles.header__logo}>
-      <img src="/img/logo.svg" alt="" />
+      <img src="img/logo.svg" alt="" />
     </a>
   );
 };
